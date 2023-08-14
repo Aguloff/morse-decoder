7319a05c28e01935f670a856fb689ab9e7c39b2a
Aguloff <agulovu@gmail.com>
2023-08-14 17:41:30 +0300

    add completed task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,54 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const exprItems = [];
+    const items = [];
+    const letters = [];
+    let result = ''; 
+ 
+    for (let i = 0; i < expr.length; i += 10) {
+      exprItems.unshift((+expr.slice(i, i + 10)) || ' ');
+    }
+  
+    for (let code of exprItems) {
+      if (typeof code === 'string') {
+        items.push([code]);
+        continue;
+      }
+      
+      code = code.toString();
+      
+      if (code.length > 2) {
+        let someItems = [];
+        
+        for (let i = 0; i < code.length; i += 2) {
+          someItems.unshift(code.slice(i, i + 2));
+        }
+        
+        items.push(someItems);
+      } else {
+        items.push([code]);
+      }
+    }
+  
+    items.forEach(item => {
+        let letterCode = '';
+
+        item.forEach(n => {
+          if (n === ' ') {
+            letterCode += ' ';
+          } else if (n === '10') {
+            letterCode += '.';
+          } else {
+            letterCode += '-';
+          }
+        });
+         letters.push(letterCode.split('').reverse().join(''));
+    });
+
+    letters.reverse().forEach(el => result += MORSE_TABLE[el] || ' ');
+    
+    return result;
 }
 
 module.exports = {
